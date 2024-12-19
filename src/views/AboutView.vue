c49323b7368342332bc8b6cd0fa48bd82db04eeb
Mateo <mateo.estrada@oregonstate.edu>
2024-12-19 15:10:38 -0800

    Updates

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about container">
-    <h1>About this tool</h1>
-    <p class="fst-italic fs-5">
-      This application's entire purpose is to show that a study guide can be
-      beneficial in rote memorization.
-    </p>
-  </div>
-</template>
